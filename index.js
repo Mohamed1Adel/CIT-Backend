30d1dfb74b4b23044479c7a7255e8e35e50652e3
Mohamed Adel <mohamedqwe542@gmail.com>
2024-02-04 19:58:05 +0200

    ي

--- index.js
+++ index.js
@@ -473,9 +473,12 @@ app.post("/login",async(req,res)=>{
 }
 
 mongoose
+  // .connect(
+  //   "mongodb+srv://mohamedqwe542:Sliman01556040246@cluster0.sx79eno.mongodb.net/?retryWrites=true&w=majority"
+  // )
   .connect(
-    
-    "mongodb+srv://admin:Cit_2024@cluster0.prld305.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://mohamedqwe542:Sliman01556040246@cluster0.im7a3nt.mongodb.net/?retryWrites=true&w=majority"
+    // "mongodb+srv://admin:Cit_2024@cluster0.prld305.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => {
     app.listen(8080 || process.env.PORT, () => {
